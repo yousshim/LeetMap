93129cc499ae55368b49d5c8284c0f4b4e271042
Yousef Ibrahim <yousef98ibrahim@gmail.com>
2023-04-06 13:42:29 +0000

    using macros

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,48 @@
-use std::collections::HashMap;
+// Define a trait for objects that can be printed
+trait Printable {
+    fn print(&self);
+}
+
+// Define a macro to generate the code for the PrintableObject enum and its implementation of the Printable trait
+macro_rules! define_printable_object {
+    // Match a list of enum variants with their associated data and printing logic
+    ($($variant:ident { $($field:ident: $type:ty),* } => $print:block),*) => {
+        // Generate the code for the PrintableObject enum
+        enum PrintableObject {
+            $($variant { $($field: $type),* }),*
+        }
+        
+        // Generate the code for the implementation of the Printable trait for PrintableObject
+        impl Printable for PrintableObject {
+            fn print(&self) {
+                match self {
+                    // Generate a match arm for each variant of the PrintableObject enum
+                    $(PrintableObject::$variant { $($field),* } => $print),*
+                }
+            }
+        }
+    }
+}
+
+// Use the define_printable_object macro to generate the code for the PrintableObject enum and its implementation of the Printable trait
+define_printable_object!(
+    RepeatedMessage { message: String, times: i32 } => {
+        for _ in 0..*times {
+            println!("{}", message);
+        }
+    },
+    Message { message: String } => {
+        println!("{}", message);
+    }
+);
 
 fn main() {
+    // Create a vector of printable objects
+    let printables: Vec<PrintableObject> = vec![
+        PrintableObject::RepeatedMessage { message: "foo".to_string(), times: 5 },
+        PrintableObject::Message { message: "my name".to_string() },
+    ];
     
-}
-
-pub fn is_anagram(s: String, t: String) -> bool {
-    if s.len() != t.len() {
-        return false;
-    }
-    let mut counts = [0; 26];
-
-    for (c1, c2) in s.chars().zip(t.chars()) {
-        counts[(c1 as u8 - b'a') as usize] += 1;
-        counts[(c2 as u8 - b'a') as usize] -= 1;
-    }
-
-    return counts.iter().all(|&x| x == 0);
-}
-
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut complements: HashMap<i32, i32> = HashMap::new();
-    for i in 0..nums.len() {
-        match complements.get(&nums[i]) {
-            Some(&x) => return vec![x, i as i32],
-            None => complements.insert(target - nums[i], i as i32),
-        };
-    }
-    panic!("should have atleast one answer");
-}
-
-pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
-    strs.into_iter()
-    .fold(std::collections::HashMap::<[i32; 26], Vec<String>>::new(), |mut groups, curr| {
-        let key = curr.bytes()
-            .map(|c| (c - b'a') as usize)
-            .fold([0; 26], |mut freqs, c| {
-                freqs[c] += 1;
-                freqs
-            });
-        groups.entry(key).or_default().push(curr);
-        groups
-    }).into_values().collect()
-}
-
-pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
-    let frequencies = nums.iter()
-        .fold(HashMap::<&i32, i32>::new(), |mut acc, x| {
-            match acc.get(x) {
-                Some(&y) => acc.insert(x, y),
-                None => acc.insert(x, 1),
-            };
-            acc
-        });
-    let mut nums_copy = nums.to_owned();
-    nums_copy.sort_by_key(|x| frequencies.get(x).unwrap());
-    nums_copy.dedup();
-    nums_copy.into_iter().take(k as usize).collect()
+    // Use an iterator to print each object using the fully qualified syntax
+    printables.iter().for_each(Printable::print);
 }
